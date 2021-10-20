4ece046027b20f07eb6ed6865a871e52adf84eb1
Gabriel Medrado <gabriel.medradoo@hotmail.com>
2021-10-20 16:12:19 -0300

    cadastro de mensagem

---
+++ src/middleware/ensureAuthenticated.ts
@@ -0,0 +1,39 @@
+import { Request, Response, NextFunction, request } from "express";
+import { verify } from "jsonwebtoken";
+/* fazendo a autenticaçao, se meu usuario nao estiver autenticado/ token invalido
+eu quero que minha aplicaçao retorne um erro, mas se estiver autenticado eu quero passar a frente
+por isso o uso do next  */
+
+interface IPayload {
+  sub: string;
+}
+
+export function ensureAuthenticated(
+  Request: Request,
+  response: Response,
+  next: NextFunction
+) {
+  const authToken = request.headers.authorization;
+
+  if (!authToken) {
+    return response.status(401).json({
+      errorCode: "token.invalid",
+    });
+  }
+
+  // bearer, a65s46a5s465s4as65a4s5646s54 => meu Bearer e token
+  /*  [0] Bearer
+        [1]  a65s46a5s465s4as65a4s5646s54 */
+
+  const [, token] = authToken.split(" ");
+
+  try {
+    const { sub } = verify(token, process.env.JWT_SECRET) as IPayload;
+
+    request.user_id = sub;
+
+    return next();
+  } catch (err) {
+    return response.status(401).json({ errorCode: "token.expired" });
+  }
+}
--- src/router.ts
+++ src/router.ts
@@ -1,11 +1,19 @@
 import { Router } from "express";
 import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
+import { CreateMessageController } from "./controllers/CreateMessageController";
+import { ensureAuthenticated } from "./middleware/ensureAuthenticated";
 
 const router = Router();
 
 //Já instaciei o controller e chamei o metodo handle
 /* este metodo recebe o req, res mas como estou usando ele dentro da minha rota ele vai 
-funcionar como um MIDWAY, por isso nao preciso passar req, res porq o express consegue fazer automatico*/
+funcionar como um MIDDLEWARE, por isso nao preciso passar req, res porq o express consegue fazer automatico*/
 router.post("/Authenticate", new AuthenticateUserController().handle);
+
+router.post(
+  "/messages",
+  ensureAuthenticated,
+  new CreateMessageController().handle
+);
 
 export { router };
