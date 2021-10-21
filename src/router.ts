dad7096dd969826a207ed33b90dbdc60983cfac2
Gabriel Medrado <gabriel.medradoo@hotmail.com>
2021-10-20 22:59:23 -0300

    criar profile user

--- src/router.ts
+++ src/router.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
 import { CreateMessageController } from "./controllers/CreateMessageController";
 import { GetLast3MessagesController } from "./controllers/GetLast3MessagesController";
+import { ProfileUserController } from "./controllers/ProfileUserController";
 import { ensureAuthenticated } from "./middleware/ensureAuthenticated";
 
 const router = Router();
@@ -19,5 +20,7 @@ router.post(
 
 //nao precisa de autenticaçao
 router.get("/messages/last3", new GetLast3MessagesController().handle);
+
+router.get("/profile", ensureAuthenticated, new ProfileUserController().handle);
 
 export { router };
