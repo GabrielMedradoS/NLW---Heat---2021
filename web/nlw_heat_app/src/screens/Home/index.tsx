5db9bd4980b84ff0ac463990c1d8edb5b26b502b
Gabriel Medrado <gabriel.medradoo@hotmail.com>
2021-11-06 13:39:16 -0300

    Components

--- web/nlw_heat_app/src/screens/Home/index.tsx
+++ web/nlw_heat_app/src/screens/Home/index.tsx
@@ -5,13 +5,14 @@ import { styles } from "./home";
 import { Header } from "../../components/Header";
 import { MessageList } from "../../components/MessageList";
 import { SignInBox } from "../../components/SignInBox";
+import { SendMessageForm } from "../../components/SendMessageForm";
 
 export function Home() {
   return (
     <View style={styles.container}>
       <Header />
       <MessageList />
-      <SignInBox />
+      <SendMessageForm />
     </View>
   );
 }
